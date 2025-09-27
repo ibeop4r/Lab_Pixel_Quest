daab88b5a31592fe3d6703da14aadf1889c2c698
ibeop4r <limebegoated@gmail.com>
2025-09-27 12:23:10 -0400

    9/27

---
+++ Assets/Geo_Quest/Scripts/geo controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geocontroller : MonoBehaviour
+{
+    string varibleOne = "hello ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string varibleTwo = "world";
+        Debug.Log("helloworld");
+        Debug.Log(varibleOne + varibleTwo);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
