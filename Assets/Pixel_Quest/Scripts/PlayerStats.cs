0f567272245b581fa0fb3a1f2aa55a13c15c2bb6
ibeop4r <limebegoated@gmail.com>
2025-11-08 12:21:09 -0500

    11/8

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlayerJump : MonoBehaviour
+public class PlayerStats : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +16,7 @@ public class PlayerJump : MonoBehaviour
     {
         
     }
-}
+}   private void OnTriggerEnter(Collideer)
+    {
+        
+    }
